4194ebf36710708169b860b417b7387927329d3c
petr_bu <petr_bu@PC00151.volga.magnat.lan>
2015-01-12 11:46:55 +0400

    inial commit

---
+++ src/org/javaprotrepticon/android/androidutils/DateUtils.java
@@ -0,0 +1,15 @@
+package org.javaprotrepticon.android.androidutils;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class DateUtils {
+	
+	public static String format(Date date) {
+		SimpleDateFormat format = new SimpleDateFormat("dd MMMM yyyy", new Locale("ru", "RU"));		
+	
+		return format.format(date);
+	}
+	
+}
