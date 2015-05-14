36dc187a58556a372a0900d1dcbfa9998950c9db
protreptic <bukhal.ps@gmail.com>
2015-05-14 16:11:38 +0400

    mini

--- src/org/javaprotrepticon/android/androidutils/Fonts.java
+++ src/org/javaprotrepticon/android/androidutils/Fonts.java
@@ -27,27 +27,6 @@ public class Fonts {
 	
 	private Fonts(Context context) {
 		mContext = context;
-
-		mDefaultTypeface = Typeface.createFromAsset(context.getAssets(), "font/RobotoCondensed-Light.ttf");
-		
-//		mTypefaces.put("Roboto-Black", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Black.ttf"));
-//		mTypefaces.put("Roboto-BlackItalic", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-BlackItalic.ttf"));
-//		mTypefaces.put("Roboto-Bold", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Bold.ttf"));
-//		mTypefaces.put("Roboto-BoldItalic", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-BoldItalic.ttf"));
-//		mTypefaces.put("RobotoCondensed-Bold", Typeface.createFromAsset(context.getAssets(), "fonts/RobotoCondensed-Bold.ttf"));
-//		mTypefaces.put("RobotoCondensed-BoldItalic", Typeface.createFromAsset(context.getAssets(), "fonts/RobotoCondensed-BoldItalic.ttf"));
-//		mTypefaces.put("RobotoCondensed-Italic", Typeface.createFromAsset(context.getAssets(), "fonts/RobotoCondensed-Italic.ttf"));
-//		mTypefaces.put("RobotoCondensed-Light", Typeface.createFromAsset(context.getAssets(), "fonts/RobotoCondensed-Light.ttf"));
-//		mTypefaces.put("RobotoCondensed-LightItalic", Typeface.createFromAsset(context.getAssets(), "fonts/RobotoCondensed-LightItalic.ttf"));
-//		mTypefaces.put("RobotoCondensed-Regular", Typeface.createFromAsset(context.getAssets(), "fonts/RobotoCondensed-Regular.ttf"));
-//		mTypefaces.put("Roboto-Italic", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Italic.ttf"));
-//		mTypefaces.put("Roboto-Light", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Light.ttf"));
-//		mTypefaces.put("Roboto-LightItalic", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-LightItalic.ttf"));
-//		mTypefaces.put("Roboto-Medium", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Medium.ttf"));
-//		mTypefaces.put("Roboto-MediumItalic", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-MediumItalic.ttf"));
-//		mTypefaces.put("Roboto-Regular", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Regular.ttf"));
-//		mTypefaces.put("Roboto-Thin", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Thin.ttf"));
-//		mTypefaces.put("Roboto-ThinItalic", Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-ThinItalic.ttf"));
 	}
 	
 	public static Fonts get(Context context) {
